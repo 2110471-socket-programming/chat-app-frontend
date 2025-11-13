52c889d36a6812ba701369646c96027520ec80ed
Pete ang Jaaa <sakayanagi101@gmail.com>
2025-11-14 04:15:55 +0700

    feat: add sender profile

--- src/chat/ChatMessage.tsx
+++ src/chat/ChatMessage.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { motion } from 'framer-motion';
-import type { ChatHistory } from '../interface/interface';
+import type { ChatHistory, User } from '../interface/interface';
 import { useUser } from '../context/UserContext';
 import {
   getGroupChatHistoryById,
@@ -12,20 +12,16 @@ import { socket } from '../config/config';
 type ChatMessageType = {
   chatId: string;
   type: 'private' | 'group';
+  clients: User[];
 };
 
-export default function ChatMessage({ chatId, type }: ChatMessageType) {
+export default function ChatMessage(
+  { chatId, type, clients }: ChatMessageType
+) {
   const [messages, setMessages] = useState<ChatHistory[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const { user } = useUser();
-  useEffect(() => {
-    window.scrollTo({
-      top: document.body.scrollHeight,
-      behavior: 'smooth',
-    });
-  }, []);
-
   useEffect(() => {
     setIsLoading(true);
     (async () => {
@@ -65,41 +61,72 @@ export default function ChatMessage({ chatId, type }: ChatMessageType) {
       return [...messages, newMessage];
     });
   }
-
+  const userMap = useMemo(
+    () => Object.fromEntries(clients.map((u) => [u._id, u])),
+    [clients],
+  );
   return (
     <>
       <div className="flex-1 overflow-y-auto mb-3 bg-white rounded-lg shadow-inner p-4 space-y-3 h-screen ">
         {isLoading ? (
           <h1>Loading...</h1>
         ) : (
-          messages.map((msg, i) => (
-            <div
-              key={i}
-              className={`flex ${msg.senderName === user.name ? 'justify-end' : 'justify-start'}`}
-            >
-            <motion.div
-              initial={{ opacity: 0, y: 10 }}
-              animate={{ opacity: 1, y: 0 }}
-              className={`p-2 rounded-lg max-w-md break-words ${
-                msg.senderName === user.name
-                  ? 'bg-blue-500 text-white'
-                  : 'bg-gray-200'
-              }`}
-              >
-                <p className="text-xs opacity-70">
-                  {msg.senderId !== user._id && msg.senderName}
-                </p>
-                {msg.type === 'text' ? (
-                  <p className="text-sm">{msg.content}</p>
-                ) : (
-                  <img
-                    src={msg.content}
-                    className="w-[150px] h-[112px] object-cover rounded-md"
-                  />
-                )}
-              </motion.div>
-            </div>
-          ))
+          <>
+            {messages.map((msg: ChatHistory, index) => {
+              const profileUrl = userMap[msg.senderId].profileUrl;
+              return (
+                <div
+                  key={index}
+                  className={`flex  ${msg.senderName === user.name ? 'justify-end' : 'justify-start'}`}
+                >
+                  {msg.senderName !== user.name && (
+                    <img
+                      src={profileUrl}
+                      alt="pfp"
+                      className="w-8 h-8 rounded-full me-5 border-1 border-gray-300"
+                    />
+                  )}
+                  {msg.senderName === user.name && (
+                    <div className="self-end text-sm mx-2">
+                      {new Date(msg.date).toLocaleTimeString([], {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                      })}
+                    </div>
+                  )}
+                  <motion.div
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    className={`p-2 rounded-lg max-w-md break-words ${
+                      msg.senderName === user.name
+                        ? 'bg-blue-500 text-white'
+                        : 'bg-gray-200'
+                    }`}
+                  >
+                    <p className="text-xs opacity-70">
+                      {msg.senderId !== user._id && msg.senderName}
+                    </p>
+                    {msg.type === 'text' ? (
+                      <p className="text-sm">{msg.content}</p>
+                    ) : (
+                      <img
+                        src={msg.content}
+                        className="w-[150px] h-[112px] object-cover rounded-md"
+                      />
+                    )}
+                  </motion.div>
+                  {msg.senderName !== user.name && (
+                    <div className="self-end text-sm mx-2">
+                      {new Date(msg.date).toLocaleTimeString([], {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                      })}
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+          </>
         )}
       </div>
       <MessageSendBox sendNewMessage={sendNewMessage} />
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-export const serverUrl = import.meta.env.SERVER_URL || 'http://localhost:3000';
+export const serverUrl = import.meta.env.SERVER_URL || 'http://localhost:5000';
 
 export const socket = io(serverUrl);
